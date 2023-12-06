5c870f777a13bc9d98c3f996199b57e62159d9ee
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2023-12-06 21:25:00 +0530

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Class1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Second class in second project.");
+            Console.ReadLine();
+        }
+    }
+}

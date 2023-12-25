6765b94102266e2a72bd8f4f13cfe49ce2c5f8c4
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2023-12-25 10:31:43 +0530

    Constructor file uploaded

---
+++ Constructor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Program
+    {
+        int i; bool b;
+        static void Main(string[] args)
+        {
+            Program p = new DemoProject.Program();
+            Console.WriteLine(p.i);
+        }
+    }
+}

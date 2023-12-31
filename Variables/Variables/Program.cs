302ff4cddb92bc2d507f9e60bb86374cf651f70c
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2023-12-31 13:24:24 +0530

    Variables in a Class
    i.  Non-Static
    ii. Static 
    iii. Constant
    iv. ReadOnly

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Variables
+{
+    class Program
+    {
+        int x;    //Non-Static
+        static int y = 200;  //Static 
+        const float pi = 3.14f;
+        readonly bool flag;
+        public Program(int x, bool flag)
+        {
+            this.x = x;
+            this.flag = flag;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(y);
+            Console.WriteLine(pi);
+            Program p1 = new Program(50, true);
+            Program p2 = new Program(150, false);
+            Console.WriteLine(p1.x + " " + p2.x );
+            Console.WriteLine(p1.flag + " " + p2.flag);
+            Console.ReadLine();
+        }
+    }
+}
+

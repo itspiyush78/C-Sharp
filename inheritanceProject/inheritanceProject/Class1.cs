554dcd36843e45798070dac6c7897f51456ed5d7
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2024-01-22 13:06:33 +0530

    inheritance and it type
    Its a mechanism of consuming the member of one class in another class by establishing parent /child relationship between between the class which provides re-usablity.
    
    types of inheritance-
    1. Single
    2. Multi-level
    3. Hirerchical
    4. Hybrid
    5. Multiple
    
    - if at all a class has immediate parent class to it we call it as single inheritance and if it has more than immediate parent class to it we call it as multiple inheritance  .

---
+++ inheritanceProject/inheritanceProject/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace inheritanceProject
+{
+    class Class1
+    {
+      public  Class1()
+        {
+            Console.WriteLine("Class1 constructor is called.");
+        }
+        public void Test1()
+        {
+            Console.WriteLine("Method 1");
+        }
+        public void Test2()
+        {
+            Console.WriteLine("Method 2");
+        }
+    }
+}
